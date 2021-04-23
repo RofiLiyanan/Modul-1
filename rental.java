57a76456962db2efad9101249c22db07d45d3c57
RofiLiyanan <82301773+RofiLiyanan@users.noreply.github.com>
2021-04-23 14:40:23 +0700

    Create rental.java

---
+++ rental.java
@@ -0,0 +1,16 @@
+package praktikum;
+
+
+public class rental {
+    public String judul,publisher;
+    int stok;
+    
+    rental(String judul,String publisher, int stok){
+        this.judul=judul;
+        this.publisher=publisher;
+        this.stok=stok;
+    }
+    rental(){
+        
+    }
+}
